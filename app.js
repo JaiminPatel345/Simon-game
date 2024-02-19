8b6e85fc253ebb50667f0087754fb6906d9a55d0
Jaimin Detroja <officialjaimin@gmail.com>
2024-02-19 23:33:12 +0530

    complate Javascript

---
+++ app.js
@@ -0,0 +1,96 @@
+let gameSeq =[];
+let userSeq=[];
+let btns = document.querySelectorAll(".box");
+let text = document.querySelector("h3");
+let start = false;
+let level = 0;
+let idx=0;
+
+document.addEventListener("keypress" , function(){
+    
+    if(start){
+        return;
+    }
+
+    start = true;
+
+    //change background color 
+    document.body.style.backgroundColor="Green";
+    setTimeout(function(){
+        document.body.style.backgroundColor = "";
+    } , 300);
+
+    console.log("Game start");
+    leverUp();  
+});
+
+function gameFlesh(num){
+
+    let box = document.querySelector(`#b${num}`);
+    console.log(num);
+    box.style.backgroundColor = "white";
+
+    setTimeout(function(){
+        box.style.backgroundColor = "";
+    } , 200);
+
+}
+
+function leverUp(){
+    level++;
+    idx =0;
+    text.innerText = `Level ${level}`;
+
+    let num = Math.floor(Math.random()*4)+1 ;
+    gameFlesh(num);  
+    gameSeq.push(`b${num}`);
+    console.log(gameSeq);
+
+}
+
+
+
+function Check(){
+    if(!start) return;
+    
+    let btn = this;
+    userSeq.push(btn.id);
+    
+
+    console.log(btn.id);
+    console.log(gameSeq[idx]);
+
+
+    if(btn.id == gameSeq[idx]){
+        idx++;
+    }else{
+        gameOver();
+        return ;
+    }
+            
+    if(idx == gameSeq.length) {
+        leverUp();
+    }
+        
+    
+}
+
+function gameOver(){
+
+    document.body.style.backgroundColor = "red";
+    setTimeout(function(){
+        document.body.style.backgroundColor = "";
+    } , 500);
+
+    text.innerText = "Press any key to RESTART the game";
+    start = false;
+    level =0;
+    gameSeq = [];
+ 
+}
+
+
+
+for(let btn of btns){
+    btn.addEventListener("click" , Check);
+}
